3068b74bc5b19b32e7a578051b2cb9a7e925d69b
DovahKiin74 <111499844+DovahKiin74@users.noreply.github.com>
2024-11-16 21:29:42 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Show contact overlay
+    document.getElementById("contactSales").addEventListener("click", function(event) {
+        event.preventDefault();
+        document.getElementById("overlay-contact").classList.remove("d-none");
+    });
+
+    // Close contact overlay
+    document.getElementById("closeContactOverlay").addEventListener("click", function() {
+        document.getElementById("overlay-contact").classList.add("d-none");
+    });
+
+    // Show signup overlay
+    document.getElementById("signupSales").addEventListener("click", function(event) {
+        event.preventDefault();
+        document.getElementById("overlay-signup").classList.remove("d-none");
+    });
+
+    // Close signup overlay
+    document.getElementById("closeSignupOverlay").addEventListener("click", function() {
+        document.getElementById("overlay-signup").classList.add("d-none");
+    });
+});
